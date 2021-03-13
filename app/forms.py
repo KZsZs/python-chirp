97939f8757c32840e808827d3c45046e0ac93275
Tamas Jozsa <tjozsa@gmail.com>
2021-03-13 16:45:37 +0100

    Closes #3 Show real posts from followers on home screen
     - Fixed missing relationship between users and posts
     - Implemented a way for people to submit posts from the UI
     - Implemented real posts to be shown from own and followed users on the home screen.

--- app/forms.py
+++ app/forms.py
@@ -51,3 +51,9 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError('Please use a different email address.')
 
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[
+        DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
+
